ab3b3028418c178b012cbc4fedcc6f3a6549ea25
shubh648 <shubhamg648@gmail.com>
2018-09-03 17:35:43 +0530

    Project Completed

--- controllers/event.js
+++
@@ -1,7 +0,0 @@
-const players = require('./playerList');
-module.exports = {
-    on:(event, ...params)=>{
-        console.log("came to event", event);
-        players[event](...params);
-    }
-}
--- controllers/playerList.js
+++ controllers/playerList.js
@@ -1,14 +1,8 @@
 var users = require('./users.json');
 const fs = require('fs');
 const app = require('../app');
-var position;
+
 module.exports = playerList = {
-
-    list: (data) => {
-
-        console.log('this is data in the playlist', data);
-        writeData(JSON.stringify(data, null, 2));
-    },
 
     readData: () => {
         return users.availableUsers;
@@ -23,35 +17,90 @@ module.exports = playerList = {
         cb(users.availableUsers);
     },
 
-    setPosition: (pos) => {
-        console.log("setPosition", pos);
-
-        position = pos;
-    },
-
-    getPosition: (cb) => {
-        console.log("getPosition", position);
-
-        cb(position);
-    },
-
     newGame: (data) => {
         writeData(data);
         app.socket.emit('newPlayer', data);
+    },
+
+    joinGame: (data) => {
+        writeCurrentPlaying(data);
+        app.socket.emit('joinGame', data);
+
+    },
+
+    multiPlayer: (position) => {
+        app.socket.emit('multiPlayer', position);
+    },
+
+    restart: (data) => {
+        app.socket.emit('restart', data);
+    },
+
+    destroy: (data) => {
+        removeFromCurrentPlaying(data);
+        app.socket.emit('destroy', data);
     }
 }
 
 function writeData(user) {
-    let jsonUsers = typeof user ==='string' ? JSON.parse(user):user;
+    let jsonUsers = typeof user === 'string' ? JSON.parse(user) : user;
 
     if (!users.availableUsers.some(e => e.player === jsonUsers.player)) {
         users.availableUsers.push(jsonUsers);
 
         fs.writeFile('./controllers/users.json', JSON.stringify(users), (err) => {
             if (err) throw err;
-            console.log('Data written to file');
+            console.log('Data written to availableUsers');
         });
     } else {
         console.log("already in file");
     }
+}
+
+function writeCurrentPlaying(players) {
+    let jsonUsers = typeof players === 'string' ? JSON.parse(players) : players;
+    let removeUsers = [];
+
+    users.usersPlaying.push(jsonUsers);
+
+    users.availableUsers.forEach((element,index) => {
+        if (element.player === players.player1 || element.player === players.player2) {
+            removeUsers.push(index);
+        }
+    })
+
+    removeUsers.sort((a, b) => { return b - a }).forEach(removeFromAvailableUsers);
+
+    fs.writeFile('./controllers/users.json', JSON.stringify(users), (err) => {
+        if (err) throw err;
+        console.log('Data written to usersPlaying');
+    });
+
+}
+
+function removeFromAvailableUsers(element) {
+        
+    users.availableUsers.splice(element, 1);
+
+}
+
+function removeFromCurrentPlaying(players) {
+    let removeUsers = [];  
+
+    users.usersPlaying.forEach((element,index) => {
+        if (element.player1 === players.player1 && element.player2 === players.player2) {
+            removeUsers.push(index);
+        }
+    })
+
+    removeUsers.sort((a, b) => { return b - a }).forEach(element => {
+        users.usersPlaying.splice(element, 1);
+    });;
+
+    fs.writeFile('./controllers/users.json', JSON.stringify(users), (err) => {
+        if (err) throw err;
+        console.log('Data written to usersPlaying');
+    });
+    
+
 }
